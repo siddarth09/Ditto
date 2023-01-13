58305fc93e0e28f5d2382ed3f56d1004ea82072b
siddarth <siddarth.intern@flomobility.com>
2023-01-13 10:14:35 +0530

    Updated QR sequence

--- ditto/scripts/qr_scanner.py
+++ ditto/scripts/qr_scanner.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
 
 import rospy
-from ditto.msg import OTP
-from camera_node import OAKD
+from std_msgs.msg import Int32
+
 import cv2
 import depthai as dai
 import numpy as np
+from time import sleep
+
+class OAKD():
+    def __init__(self):
+        pass
+    
+    def getFrame(self,queue):
+        # Get frame from queue
+        self.frame = queue.get()
+        # Convert frame to OpenCV format and return
+        return self.frame.getCvFrame()
+        
+    def getMonoCamera(self,pipeline):
+        # Configure mono camera
+        mono = pipeline.createMonoCamera()
+
+        # Set Camera Resolution
+        mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
+        mono.setBoardSocket(dai.CameraBoardSocket.RGB)
+        
+        return mono
+
+    def getColorCam(self,pipeline):
+        rgb=pipeline.create(dai.node.ColorCamera)
+        rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+        
+        return rgb
 
 class QR():
     def __init__(self):
-        self.otp= rospy.Subscriber('/otp',OTP,self.get_data)
+        
+        self.passcode=0
+        self.qrdata=0
+        rospy.Subscriber('/otp',Int32,callback=self.get_data)
 
     def get_camera(self):
         camera=OAKD()
@@ -27,30 +57,44 @@ class QR():
 
             # Get output queues. 
             Queue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-            
+            detector = cv2.QRCodeDetector()
             # Set display window name
             
             while True:
                 # Get left frame
                 Frame = camera.getFrame(Queue)
-                #rospy.loginfo("USB SPEED= {}".format(device.getUsbSpeed().name))
-                # Display output image
-                cv2.imshow("QR SCANNER",Frame)
-                
-                # Check for keyboard input
-                key = cv2.waitKey(1)
-                if key == ord('q'):
-                    # Quit when q is pressed
+                data, bbox, _ = detector.detectAndDecode(Frame)
+                # check if there is a QRCode in the image
+                if data:
+                    rospy.loginfo("QRCODE FOUND")
+                    sleep(3)
+                    self.qrdata=data
                     break
-                
+                else:
+                    rospy.loginfo("NO QRCode found")
+                self.qrdata=data
+                cv2.imshow("QRCODEscanner", Frame)    
+                if cv2.waitKey(1) == ord("q"):
+                    break           
+
     def get_data(self,msg):
-        self.passcode=msg.password
         
-        
+        self.passcode=msg.data
 
-
+    def verify(self):
+        self.get_camera()
+        if (self.passcode == self.qrdata):
+            rospy.loginfo(self.passcode)
+            rospy.loginfo(self.qrdata)
+            rospy.loginfo("SUCCESS")
+        else:
+            rospy.loginfo(self.passcode)
+            rospy.loginfo(self.qrdata)
+            rospy.loginfo("FAILURE")
 
 if __name__=="__main__":
-
+    rospy.init_node('qr_scan')
     scanner=QR()
-    scanner.get_camera()
+    scanner.verify()
+    
+    
